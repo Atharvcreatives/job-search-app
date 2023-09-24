8eeedbf5c4f7975abe5d97b1f0a27e4be5a4b9ab
Atharv Gour <atharvexe@gmail.com>
2023-09-24 12:01:26 +0530

    Home page setup done

--- app/index.js
+++ app/index.js
@@ -11,9 +11,33 @@ const Home = () =>{
   <SafeAreaView style={{flex:1 , backgroundColor: COLORS.lightWhite}}>
     <Stack.Screen
       options={{
-        headerStyle:{backgroundColor: COLORS.lightWhite}
+        headerStyle:{backgroundColor: COLORS.lightWhite},
+        headerShadowVisible: false,
+        headerLeft: ()=>(
+          <ScreenHeaderBtn iconURL ={icons.menu} dimension="60%"/>
+        ),
+        headerRight: ()=>(
+          <ScreenHeaderBtn iconURL ={icons.profile} dimension="100%"/>
+        ),
+        headerTitle: ""
       }}
     />
+
+    <ScrollView showsVerticalScrollIndicator={false}>
+    <View 
+    style={{
+      flex: 1,
+      padding : SIZES.medium
+    }}
+    >
+      <Welcome 
+      
+      />
+
+      <Popularjobs />
+      <Nearbyjobs />
+    </View>
+    </ScrollView>
   </SafeAreaView>
  )
 }
